d530d465796dbb5cb91f2fcc3392e3782218100b
Shilpa <shilpamjoy@gmail.com>
2021-08-03 23:01:41 -0400

    Initial Commit

---
+++ SlotApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SlotApp
+//
+//  Created by Shilpa Joy on 2021-08-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
